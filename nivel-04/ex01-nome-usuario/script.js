c12cac7a476bea2199b9d71cb30d4683fbd1c258
Emanuel Quintino <92856419+EmanuelQuintino@users.noreply.github.com>
2022-07-13 15:07:45 -0300

    Exercicio de calculadora simples

--- nivel-04/ex01-nome-usuario/script.js
+++ nivel-04/ex01-nome-usuario/script.js
@@ -1,5 +1,5 @@
 // Pergunte o nome do usuário e escreva a mensagem: "Olá, [usuário]!"
 
-let name = prompt('Digite seu nome: ')
+let name = prompt("Digite seu nome: ")
 
-alert(`Olá ${name}`)
+alert("Olá, " + name)
---
+++ nivel-04/ex02-media/script.js
@@ -0,0 +1,13 @@
+// Solicite 2 números e apresente a soma deles
+// Depois apresente as outras operações simples
+
+let numberOne = Number(prompt("Digite o primeiro número: "))
+let numberTwo = Number(prompt("Digite o sugundo número: "))
+
+alert(`A soma de ${numberOne} e ${numberTwo} = ${numberOne + numberTwo}`)
+
+alert(`A subtração de ${numberOne} e ${numberTwo} = ${numberOne - numberTwo}`)
+
+alert(`A multiplicação de ${numberOne} e ${numberTwo} = ${numberOne * numberTwo}`)
+
+alert(`A divisão de ${numberOne} e ${numberTwo} = ${numberOne / numberTwo}`)
