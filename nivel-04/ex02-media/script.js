0c6034713f7f5f4ee5cddeccf5994ebb8cf13c42
Emanuel Quintino <92856419+EmanuelQuintino@users.noreply.github.com>
2022-07-13 15:16:15 -0300

    Operadores matematicos

--- nivel-04/ex02-media/script.js
+++ nivel-04/ex02-media/script.js
@@ -4,10 +4,19 @@
 let numberOne = Number(prompt("Digite o primeiro número: "))
 let numberTwo = Number(prompt("Digite o sugundo número: "))
 
+
+const sum = numberOne + numberTwo
+const sub = numberOne - numberTwo
+const mul = numberOne * numberTwo
+const div = numberOne / numberTwo
+const res = numberOne % numberTwo
+
 alert(`A soma de ${numberOne} e ${numberTwo} = ${numberOne + numberTwo}`)
 
-alert(`A subtração de ${numberOne} e ${numberTwo} = ${numberOne - numberTwo}`)
+alert(`A subtração de ${numberOne} e ${numberTwo} = ${sub}`)
 
-alert(`A multiplicação de ${numberOne} e ${numberTwo} = ${numberOne * numberTwo}`)
+alert(`A multiplicação de ${numberOne} e ${numberTwo} = ${mul}`)
 
-alert(`A divisão de ${numberOne} e ${numberTwo} = ${numberOne / numberTwo}`)
+alert(`A divisão de ${numberOne} e ${numberTwo} = ${div}`)
+
+alert(`O resto da divisão de ${numberOne} e ${numberTwo} = ${res}`)
