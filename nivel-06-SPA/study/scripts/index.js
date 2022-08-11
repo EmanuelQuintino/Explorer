56adef5c2a41a085c2199553b78026436df42c2b
EmanuelQuintino <emanuelquintino@hotmail.com>
2022-08-11 17:37:01 -0300

    update path routers

--- nivel-06-SPA/study/scripts/index.js
+++ nivel-06-SPA/study/scripts/index.js
@@ -1,12 +1,12 @@
 import {Router} from "./route.js"
 
 const router = new Router()
-router.add("/nivel-06/study/", "/nivel-06/study/pages/home.html"),
-router.add("/nivel-06/study/index.html", "/nivel-06/study/pages/home.html"),
-router.add("/", "/nivel-06/study/pages/home.html"),
-router.add("/about", "/nivel-06/study/pages/about.html"),
-router.add("/contact", "/nivel-06/study/pages/contact.html"),
-router.add(404, "/nivel-06/study/pages/404.html")
+router.add("/nivel-06-SPA/study/", "/nivel-06-SPA/study/pages/home.html"),
+router.add("/nivel-06-SPA/study/index.html", "/nivel-06-SPA/study/pages/home.html"),
+router.add("/", "/nivel-06-SPA/study/pages/home.html"),
+router.add("/about", "/nivel-06-SPA/study/pages/about.html"),
+router.add("/contact", "/nivel-06-SPA/study/pages/contact.html"),
+router.add(404, "/nivel-06-SPA/study/pages/404.html")
 
 router.handle()
 
