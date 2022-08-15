20cd9a5cbc6f7c536bd3343257e9434b9c633eef
EmanuelQuintino <emanuelquintino@hotmail.com>
2022-08-15 19:01:02 -0300

    config background-image and  link colors

--- level-06-SPA/challenges/scripts/index.js
+++ level-06-SPA/challenges/scripts/index.js
@@ -1,31 +1,5 @@
 import {Router} from "./route.js"
 
-
-let linkHome = document.querySelector('ul li:nth-child(1)')
-let linkUniverse = document.querySelector('ul li:nth-child(2)')
-let linkExploration = document.querySelector('ul li:nth-child(3)')
-let page = document.querySelector('.page')
-
-
-linkHome.addEventListener("click", () => {
-    linkHome.innerHTML = '<li><a href="/" class="select" onclick="route()">Home</a></li>'
-    linkUniverse.innerHTML = '<li><a href="/universe" onclick="route()">Universe</a></li>'
-    linkExploration.innerHTML = '<li><a href="/exploration" onclick="route()">Exploretion</a></li>'
-})
-
-linkUniverse.addEventListener("click", () => {
-    linkHome.innerHTML = '<li><a href="/" onclick="route()">Home</a></li>'
-    linkUniverse.innerHTML = '<li><a href="/universe" class="select" onclick="route()">Universe</a></li>'
-    linkExploration.innerHTML = '<li><a href="/exploration" onclick="route()">Exploretion</a></li>'
-})
-
-linkExploration.addEventListener("click", () => {
-    linkHome.innerHTML = '<li><a href="/" onclick="route()">Home</a></li>'
-    linkUniverse.innerHTML = '<li><a href="/universe" onclick="route()">Universe</a></li>'
-    linkExploration.innerHTML = '<li><a href="/exploration" class="select" onclick="route()">Exploretion</a></li>'
-})
-
-
 const router = new Router()
 router.add("/level-06-SPA/challenges/", "/level-06-SPA/challenges/pages/home.html"),
 router.add("/level-06-SPA/challenges/index.html", "/level-06-SPA/challenges/pages/home.html"),
--- level-06-SPA/challenges/scripts/route.js
+++ level-06-SPA/challenges/scripts/route.js
@@ -19,6 +19,37 @@ export class Router {
         // Desestruturando o objeto {propriedadrot x, propriedade y, propriedade z}
         const {pathname} = window.location 
         // console.log(pathname)
+
+        let myPage = document.querySelector('.page')
+        let linkHome = document.querySelector('ul li:nth-child(1)')
+        let linkUniverse = document.querySelector('ul li:nth-child(2)')
+        let linkExploration = document.querySelector('ul li:nth-child(3)')
+
+        switch (pathname) {
+            case '/':
+                linkHome.innerHTML = '<li><a href="/" class="select" onclick="route()">Home</a></li>'
+                linkUniverse.innerHTML = '<li><a href="/universe" onclick="route()">Universe</a></li>'
+                linkExploration.innerHTML = '<li><a href="/exploration" onclick="route()">Exploretion</a></li>'
+                myPage.style.backgroundImage = "url('/level-06-SPA/challenges/images/mountains-universe-1.png')"
+                break;
+            
+            case '/universe':
+                linkHome.innerHTML = '<li><a href="/" onclick="route()">Home</a></li>'
+                linkUniverse.innerHTML = '<li><a href="/universe" class="select" onclick="route()">Universe</a></li>'
+                linkExploration.innerHTML = '<li><a href="/exploration" onclick="route()">Exploretion</a></li>'
+                myPage.style.backgroundImage = "url('/level-06-SPA/challenges/images/mountains-universe-2.png')"
+                break;
+            
+            case '/exploration':
+                linkHome.innerHTML = '<li><a href="/" onclick="route()">Home</a></li>'
+                linkUniverse.innerHTML = '<li><a href="/universe" onclick="route()">Universe</a></li>'
+                linkExploration.innerHTML = '<li><a href="/exploration" class="select" onclick="route()">Exploretion</a></li>'
+                myPage.style.backgroundImage = "url('/level-06-SPA/challenges/images/mountains-universe-3.png')"
+                break;
+        
+            default:
+                break;
+        }
         
         const route = this.routes[pathname] || this.routes[404]
         
@@ -34,4 +65,3 @@ export class Router {
     
     }    
 }
-
