bd611dff488730e93bcdb8f3f664127cc8692dad
EmanuelQuintino <emanuelquintino@hotmail.com>
2022-08-15 22:36:19 -0300

    fixed link and create folders

--- level-06-Promises/SPA/challenges/scripts/index.js
+++ level-06-Promises/SPA/challenges/scripts/index.js
@@ -2,7 +2,7 @@ import {Router} from "./route.js"
 
 const router = new Router()
 router.add("/level-06-Promises/SPA/challenges/", "/level-06-Promises/SPA/challenges/pages/home.html"),
-router.add("/level-06-Promises/SPA/challenges/index.html", "/level-06-SPA/SPA/challenges/pages/home.html"),
+router.add("/level-06-Promises/SPA/challenges/index.html", "/level-06-Promises/SPA/challenges/pages/home.html"),
 router.add("/", "/level-06-Promises/SPA/challenges/pages/home.html"),
 router.add("/universe", "/level-06-Promises/SPA/challenges/pages/universe.html"),
 router.add("/exploration", "/level-06-Promises/SPA/challenges/pages/exploration.html"),
