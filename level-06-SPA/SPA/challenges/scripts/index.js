5c3acc3db5bfa5690f22791111a06e5333529ad7
EmanuelQuintino <emanuelquintino@hotmail.com>
2022-08-15 22:03:47 -0300

    update name folders

---
+++ level-06-SPA/SPA/challenges/scripts/index.js
@@ -0,0 +1,14 @@
+import {Router} from "./route.js"
+
+const router = new Router()
+router.add("/level-06-SPA/SPA/challenges/", "/level-06-SPA/SPA/challenges/pages/home.html"),
+router.add("/level-06-SPA/SPA/challenges/index.html", "/level-06-SPA/SPA/challenges/pages/home.html"),
+router.add("/", "/level-06-SPA/SPA/challenges/pages/home.html"),
+router.add("/universe", "/level-06-SPA/SPA/challenges/pages/universe.html"),
+router.add("/exploration", "/level-06-SPA/SPA/challenges/pages/exploration.html"),
+router.add(404, "/level-06-SPA/SPA/challenges/pages/404.html")
+
+router.handle()
+
+window.onpopstate = () => {router.handle()}
+window.route = () => {router.route()}
