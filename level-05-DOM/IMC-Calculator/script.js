96a3b2057ccd263a4511b7b3bca7a9ee012c2df6
EmanuelQuintino <emanuelquintino@hotmail.com>
2022-08-17 20:28:48 -0300

    add modal > onsubmit > function IMC > button close

--- level-05-DOM/IMC-Calculator/script.js
+++ level-05-DOM/IMC-Calculator/script.js
@@ -1,1 +1,26 @@
-// Painel interativo onde faço o controle e análise dos dados de abate com total, peso e média mais série histórica por categorias de faixa etária, espécie, sexo e local. Também uso para criação de relatórios e tomada de decisão nos processos.
+const form = document.querySelector("form")
+const inputWeight = document.querySelector("#weight")
+const inputHeight = document.querySelector("#height")
+const modalWrapper = document.querySelector(".modal-wrapper")
+const modalMessage = document.querySelector(".resultIMC")
+const modalClose = document.querySelector(".close")
+
+
+function IMC(weight, height) {
+    return (weight / ((height / 100) ** 2))
+}
+
+form.onsubmit = function(event) {
+    event.preventDefault()
+    
+    const weight = inputWeight.value
+    const height = inputHeight.value
+    const result = IMC(weight, height)
+    
+    modalWrapper.classList.add("open")
+    modalMessage.innerHTML = `Seu IMC é de ${result}`
+}
+
+modalClose.onclick = () => {
+    modalWrapper.classList.remove("open")
+}
