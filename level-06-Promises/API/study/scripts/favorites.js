231839cb0ec8a7abed611e896cb27d9dfcef97ad
EmanuelQuintino <emanuelquintino@hotmail.com>
2022-08-25 19:14:37 -0300

    create array users

--- level-06-Promises/API/study/scripts/favorites.js
+++ level-06-Promises/API/study/scripts/favorites.js
@@ -16,7 +16,46 @@ export class FavoritesView extends Favorites {
 
     update() {
        this.removeAllTr()
+
+       const users = [
+            {
+                login: "EmanuelQuintino",
+                name: "Emanuel Quintino",
+                public_repos: "10",
+                followers: "120"
+            },
+            {
+                login: "maykbrito",
+                name: "Mayk Brito",
+                public_repos: "10",
+                followers: "120"
+            },
+        ]
+
+        users.forEach(user => {
+            console.log(user)
+        })
     }
+
+   createRow() {
+
+    const tr = document.querySelector("tr") 
+    tr.innerHTML = `
+    <td class="users">
+        <img src="https://github.com/EmanuelQuintino.png" alt="">
+        <a href="https://github.com/EmanuelQuintino" target="_blank">
+            <p>EmanuelQuintino</p>
+            <span>Emanuel Quintino</span>
+        </a>
+    </td>
+    <td class="repositories">10</td>
+    <td class="followers">234</td>
+    <td>
+        <button class="remove">&times;</button>
+    </td>`
+
+    return tr
+   }
 
     removeAllTr() {
         const tbody = this.root.querySelector("table tbody")
