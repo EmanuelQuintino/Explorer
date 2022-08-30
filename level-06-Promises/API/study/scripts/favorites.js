54f3566621e51a6b6f76169394e28d61ac72dad8
EmanuelQuintino <emanuelquintino@hotmail.com>
2022-08-30 20:02:32 -0300

    add try user exists

--- level-06-Promises/API/study/scripts/favorites.js
+++ level-06-Promises/API/study/scripts/favorites.js
@@ -22,8 +22,14 @@ export class Favorites {
         // GithubUser.search("EmanuelQuintino").then(user => console.log(user))
     }
 
-    async add(username) {
+    async add(username) {        
         try {
+            const userExists = this.entries.find(entry => entry.login === username)
+            
+            if(userExists) {
+                throw new Error("Usuário já cadastrado")
+            }
+
             const user = await GithubUser.search(username)
             if(user.login === undefined) {
                 throw new Error("Usuário não encontrado")
