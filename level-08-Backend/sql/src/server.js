f30f2934d5e40270f254f8f04a1e9adbd7a87b29
EmanuelQuintino <emanuelquintino@hotmail.com>
2022-09-17 17:37:59 -0300

    add route params

--- level-08-Backend/sql/src/server.js
+++ level-08-Backend/sql/src/server.js
@@ -1,5 +1,12 @@
 const express = require("express")
 const app = express();
+
+app.get("/message/:id/:user", (request, response) => {
+    const {id, user} = request.params
+
+    response.send(`Olá, o id de ${user} é ${id}`)
+})
+
 const PORT = 3333
 
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
