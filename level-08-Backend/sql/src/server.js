8472d444076bd23a1d66fe73c5caada5f2b7bffe
EmanuelQuintino <emanuelquintino@hotmail.com>
2022-09-17 17:47:01 -0300

    add query params

--- level-08-Backend/sql/src/server.js
+++ level-08-Backend/sql/src/server.js
@@ -7,6 +7,12 @@ app.get("/message/:id/:user", (request, response) => {
     response.send(`Olá, o id de ${user} é ${id}`)
 })
 
+app.get("/users", (request, response) => {
+    const {page, limit} = request.query
+
+    response.send(`Página: ${page}. Mostrar ${limit}`)
+})
+
 const PORT = 3333
 
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
