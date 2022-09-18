15c98100f5110b155313489c4afb1d66a1608848
EmanuelQuintino <emanuelquintino@hotmail.com>
2022-09-18 09:54:08 -0300

    add method post request

--- level-08-Backend/sql/src/server.js
+++ level-08-Backend/sql/src/server.js
@@ -1,16 +1,26 @@
 const express = require("express")
 const app = express();
 
-app.get("/message/:id/:user", (request, response) => {
-    const {id, user} = request.params
+app.use(express.json())
 
-    response.send(`Olá, o id de ${user} é ${id}`)
-})
+// app.get("/message/:id/:user", (request, response) => {
+//     const {id, user} = request.params
 
-app.get("/users", (request, response) => {
-    const {page, limit} = request.query
+//     response.send(`Olá, o id de ${user} é ${id}`)
+// })
 
-    response.send(`Página ${page} linha ${limit}`)
+// app.get("/users", (request, response) => {
+//     const {page, limit} = request.query
+
+//     response.send(`Página ${page} linha ${limit}`)
+// })
+
+app.post("/users", (request, response) => {
+    const {name, email, password} = request.body;
+
+    // response.send(`Você chamou o POST! Name: ${name}, E-mail: ${email}, Password: ${password}`)
+
+    response.json({name, email, password})
 })
 
 const PORT = 3333
