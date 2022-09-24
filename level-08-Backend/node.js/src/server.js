e24f3889332b989447edb4d9ad74b6b8db310694
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-09-24 18:33:22 -0300

    add folder > file migrations

--- level-08-Backend/node.js/src/server.js
+++ level-08-Backend/node.js/src/server.js
@@ -1,6 +1,6 @@
 require("express-async-errors")
 
-const database = require("./database/sqlite")
+const migrationsRun = require("./database/sqlite/migrations")
 
 const AppError = require("./utils/AppError")
 
@@ -11,11 +11,11 @@ const app = express();
 
 const routes = require("./routes")
 
+migrationsRun()
+
 app.use(express.json())
 
 app.use(routes)
-
-database()
 
 app.use((error, request, response, next) => {
     if(error instanceof AppError) {
