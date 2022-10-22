03104733262719d630268e11cc521c2a715fb10b
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-10-22 12:55:01 -0300

    add tables tags and links

---
+++ level-08-Backend/node.js/src/database/knex/migrations/20221022151721_createLinks.js
@@ -0,0 +1,12 @@
+const { default: knex } = require("knex");
+
+exports.up = knex => knex.schema.createTable("links", table => {
+    table.increments("id");
+    table.text("url").notNullable;
+
+    table.integer("note_id").references("id").inTable("notes").onDelete("CASCADE");
+    
+    table.timestamp("created_at").default(knex.fn.now());
+});
+
+exports.down = knex => knex.schema.dropTable("links");
