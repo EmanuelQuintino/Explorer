f78dd7cdf6afa45386368d6a117e5234f00fce75
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-10-22 11:18:02 -0300

    add migrations knex node

--- level-08-Backend/node.js/knexfile.js
+++ level-08-Backend/node.js/knexfile.js
@@ -6,6 +6,11 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
     },
+    
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+    },
+    
     useNullAsDefault: true
-  }
+  },
 };
