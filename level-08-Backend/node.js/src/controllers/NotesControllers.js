082d3c06ab1ea3cd07e54d274490dd35e6d7ed70
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-10-29 14:47:26 -0300

    add list tags

--- level-08-Backend/node.js/src/controllers/NotesControllers.js
+++ level-08-Backend/node.js/src/controllers/NotesControllers.js
@@ -56,14 +56,43 @@ class NotesControllers{
     }
 
     async index(request, response) {
-        const {title, user_id} = request.query;
+        const {title, user_id, tags} = request.query;
 
-        const notes = await knex("notes")
+        let notes;
+
+        if(tags){
+            const filterTags = tags.split(",").map(tag => tag.trim());
+
+            notes = await knex("tags")
+                .select([
+                    "notes.id",
+                    "notes.title",
+                    "notes.user_id",
+                ])
+                .where("notes.user_id", user_id)
+                .whereLike("notes.title", `%${title}%`)
+                .whereIn("name", filterTags)
+                .innerJoin("notes", "notes.id", "tags.note_id")
+                .orderBy("notes.title")
+        }
+        else {
+            notes = await knex("notes")
             .where({user_id})
             .whereLike("title", `%${title}%`)
             .orderBy("title");
+        }
 
-        return response.json(notes);
+        const userTags = await knex("tags").where({user_id});
+        const notesWithTags = notes.map(note => {
+            const noteTags = userTags.filter(tag => tag.note_id === note.id);
+
+            return {
+                ...note,
+                tags: noteTags
+            }
+        });
+
+        return response.json(notesWithTags);
     }
 }
 
