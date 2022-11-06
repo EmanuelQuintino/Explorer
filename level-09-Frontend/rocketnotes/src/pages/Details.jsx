e25007419089785e6ac78c9c018775e80f46668a
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-11-06 16:01:25 -0300

    add styled components

--- level-09-Frontend/rocketnotes/src/pages/Details.jsx
+++
@@ -1,6 +0,0 @@
-export function Details() {
-
-  return (
-    <h1>Teste1</h1>
-  )
-} 
---
+++ level-09-Frontend/rocketnotes/src/pages/Details/index.jsx
@@ -0,0 +1,13 @@
+import { Container } from "./styles.js";
+
+export function Details() {
+
+  return (
+    <Container>
+      <h1>Teste1</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni vitae, id dolores quasi illo, sequi delectus tempora, exercitationem recusandae iure placeat animi cumque quis? Suscipit accusantium soluta iste assumenda at.
+      </p>
+    </Container>
+  )
+} 
