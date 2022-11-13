397a931e8b8f6428b0bf2921994d6a00a17968b7
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-11-12 21:43:46 -0300

    add component Header

---
+++ level-09-Frontend/rocketnotes/src/components/Header/index.jsx
@@ -0,0 +1,21 @@
+import { RiShutDownLine } from "react-icons/ri"
+import { Container, Profile, Logout } from "./styles"
+
+export function Header() {
+    return (
+        <Container>
+            <Profile>
+                <img src="https://github.com/EmanuelQuintino.png" alt="foto do usuário" />
+
+                <div>
+                    <span>Bem-vindo,</span>
+                    <strong>Emanuel Quintino</strong>
+                </div>
+            </Profile>
+
+            <Logout>
+                <RiShutDownLine/>
+            </Logout>
+        </Container>
+    )
+}
--- level-09-Frontend/rocketnotes/src/pages/Details/index.jsx
+++ level-09-Frontend/rocketnotes/src/pages/Details/index.jsx
@@ -1,19 +1,15 @@
 import { Container } from "./styles.js";
+
+
+import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
 
 export function Details() {
 
   return (
     <Container>
-      <h1>Teste1</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni vitae, id dolores quasi illo, sequi delectus tempora, exercitationem recusandae iure placeat animi cumque quis? Suscipit accusantium soluta iste assumenda at.
-      </p>
-
-      <Button title="Login" loading />
-      <Button title="Cadastrar" />
+      <Header/>
       <Button title="Voltar" />
-
     </Container>
   )
 } 
