a01f2730dc2c0761bed19672159aea976ae0bac8
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-11-13 11:40:18 -0300

    add component Tag

--- level-09-Frontend/rocketnotes/src/pages/Details/index.jsx
+++ level-09-Frontend/rocketnotes/src/pages/Details/index.jsx
@@ -4,6 +4,7 @@ import { Container, Links } from "./styles.js";
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
 import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
 
 
 export function Details() {
@@ -21,6 +22,11 @@ export function Details() {
             <a href="#">https://www.rocketseat.com.br/</a>
           </li>
         </Links>      
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express"/>
+        <Tag title="node.js"/>
       </Section>
 
       <Button title="Voltar" />
