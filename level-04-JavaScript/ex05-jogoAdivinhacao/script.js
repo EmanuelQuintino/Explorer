b9fbf9ede812b6eb239f01e4123e60e2657d2fde
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-11-20 10:10:55 -0300

    updade spaces

--- level-04-JavaScript/ex05-jogoAdivinhacao/script.js
+++ level-04-JavaScript/ex05-jogoAdivinhacao/script.js
@@ -1,11 +1,10 @@
 // Apresente a mensagem para o usuário: Adivinhe um número de 0 a 10
 // Crie uma lógica que gera um número aleatório e compare com o que foi digitado
-//Enquanto o usuário errar o número mostre: Errou! Tente novamente: 
-//Se acertar apresente a mensagem: Parabens! Acertou em N tentativas.
+// Enquanto o usuário errar o número mostre: Errou! Tente novamente: 
+// Se acertar apresente a mensagem: Parabens! Acertou em N tentativas.
 
 let numberUser = Number(prompt("Adivinhe um número de 0 a 10: "))
 const numberRamdom = Math.round(Math.random() * 10)
-
 
 let countTry = 1
 while(numberUser != numberRamdom) {
