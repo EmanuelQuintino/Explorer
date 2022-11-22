9968baa294a087e9ec488d89520978b7849eb830
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-11-22 10:45:53 -0300

    add page new header and input title

--- level-09-Frontend/rocketnotes/src/main.jsx
+++ level-09-Frontend/rocketnotes/src/main.jsx
@@ -5,13 +5,13 @@ import GlobalStyle from './styles/global'
 
 import theme from './styles/theme';
 
-import { Profile } from './pages/Profile';
+import { New } from './pages/New';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme = {theme}>
       <GlobalStyle />
-      <Profile />
+      <New />
     </ThemeProvider>
   </React.StrictMode>
 );
---
+++ level-09-Frontend/rocketnotes/src/pages/New/index.jsx
@@ -0,0 +1,22 @@
+import { Header } from "../../components/Header"
+import { Container, Form } from "./styles"
+import { Input } from "../../components/Input"
+
+export function New() {
+    return (
+        <Container>
+            <Header/>
+
+            <main>
+                <Form>
+                    <header>
+                    <h1>Criar notas</h1>
+                    <a href="/">Voltar</a>
+                    </header>
+
+                    <Input placeholder="Título"/>
+                </Form>
+            </main>
+        </Container>
+    )
+}
