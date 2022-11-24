5ee98932d20b5758e88327737972f10835238811
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-11-24 17:05:13 -0300

    add components note item

--- level-09-Frontend/rocketnotes/src/pages/New/index.jsx
+++ level-09-Frontend/rocketnotes/src/pages/New/index.jsx
@@ -2,6 +2,9 @@ import { Header } from "../../components/Header"
 import { Container, Form } from "./styles"
 import { Input } from "../../components/Input"
 import { TextArea } from "../../components/TextArea"
+import { NoteItem } from "../../components/NoteItem"
+import { Section } from "../../components/Section"
+import { Button } from "../../components/Button"
 
 export function New() {
     return (
@@ -17,6 +20,20 @@ export function New() {
 
                     <Input placeholder="Título"/>
                     <TextArea placeholder="Observações"/>
+
+                    <Section title='Links Úteis'>
+                        <NoteItem value='https://rocketseat.com.br'/>
+                        <NoteItem isNew placeholder='Novo link'/>
+                    </Section>
+
+                    <Section title='Marcadores'>
+                        <div className="tags">
+                        <NoteItem value='react'/>
+                        <NoteItem isNew placeholder='Nova tag'/>
+                        </div>
+                    </Section>
+
+                    <Button title='Salvar'/>
                 </Form>
             </main>
         </Container>
